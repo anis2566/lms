43c0752411c5d8e85a6a4023694d99bae1364314
anis2566 <anichuranis1000@gmail.com>
2024-03-19 15:56:44 +0600

    hello

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/uploadthing", "/api/webhook"],
-  apiRoutes: ["/api/uploadthing", "/api/webhook"],
-  ignoredRoutes: ["/api/webhook"],
+  publicRoutes: ["/api/uploadthing", "/api/webhooks"],
+  apiRoutes: ["/api/uploadthing", "/api/webhooks"],
+  ignoredRoutes: ["/api/webhooks"],
 });
 
 export const config = {
